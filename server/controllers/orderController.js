a7eb771eb75e2f74d37847df8ddd9c431120caa4
Shubham Vishwakarma <no_reply@gmail.com>
2025-05-30 16:18:52 +0530

    fix req and res typos

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -109,17 +109,17 @@ export const stripeWebhooks = async (req, res) => {
   // Stripe Gateway Initialize
   const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
 
-  const sig = request.headers["stripe-signature"];
+  const sig = req.headers["stripe-signature"];
   let event;
 
   try {
     event = stripeInstance.webhooks.constructEvent(
-      request.body,
+      req.body,
       sig,
       process.env.STRIPE_WEBHOOK_SECRET
     );
   } catch (error) {
-    response.status(400).send(`Webhook Error: ${error.message}`);
+    res.status(400).send(`Webhook Error: ${error.message}`);
   }
 
   // Handle the event
